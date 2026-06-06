d7e538ae75b79cd532026b67b52ed83853871361
flawada <e.sauermann@icloud.com>
2026-06-06 19:22:11 +0200

    Refactor setup.sh for Fedora compatibility checks

--- setup.sh
+++ setup.sh
@@ -1,1 +1,53 @@
-if ! grep -q '^ID=fedora' /etc/os-release; then exit;fi
+set -e
+local_path=~/.cache/flawado
+
+if ! grep -q '^ID=fedora' /etc/os-release; then
+  echo "! Warning ! : This script is made for fedora everthing"
+  read -p "Continure? [y/n]: " p
+  case "$p" in
+  [yY])
+    echo "continure text"
+    ;;
+  *)
+    echo "Exit text"
+    exit
+    ;;
+  esac
+fi
+
+sudo dnf in -y git
+mkdir -p $local_path
+cd $local_path
+
+if [ -z "$(ls -A)" ]; then
+  git clone https://github.com/flawada/mango
+fi
+
+set -e
+local_path=~/.cache/flawado
+
+if ! grep -q '^ID=fedora' /etc/os-release; then
+  echo "! Warning ! : This script is made for fedora everthing"
+  read -p "Continure? [y/n]: " p
+  case "$p" in
+  [yY])
+    echo "continure text"
+    ;;
+  *)
+    echo "Exit text"
+    exit
+    ;;
+  esac
+fi
+
+sudo dnf in -y git || true
+mkdir -p $local_path
+cd $local_path
+
+if ! [ -z "$(ls -A)" ]; then
+  git clone https://github.com/flawada/mango
+fi
+
+cd $local_path/mango
+
+./install.sh
