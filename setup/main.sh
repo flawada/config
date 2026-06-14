9bafa20084fae7aaf7012f6d7b09968577077f8a
flawada <e.sauermann@icloud.com>
2026-06-14 23:55:27 +0200

    main.sh

--- setup/main.sh
+++ setup/main.sh
@@ -2,9 +2,11 @@ cd /tmp/blueprint/blueprints
 
 
 blueprints=()
+i = 0
 
 for blueprint in */; do
-    blueprints+=("$blueprint")
+  i+=1
+  blueprints+=("$i ) $blueprint")
 done
 
-echo blueprint
+echo $blueprint
