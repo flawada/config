0611857db20925e99bbe6b440649e278f7d041b8
flawada <e.sauermann@icloud.com>
2026-06-23 15:50:49 +0200

    setup cleanup

--- setup.sh
+++ setup.sh
@@ -9,6 +9,23 @@ BLUE='\033[0;34m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
+#####
+
+clear
+
+printf "$(cat << EOF
+${BLUE}                               
+ ▄▄▄▄ ▄▄▄▄▄ ▄▄▄▄▄▄ ▄▄ ▄▄ ▄▄▄▄  
+███▄▄ ██▄▄    ██   ██ ██ ██▄█▀ 
+▄▄██▀ ██▄▄▄   ██   ▀███▀ ██
+${NC}
+EOF
+)"
+
+printf "\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n"
+
+printf "%bChecking System..%b\n" "$BLUE" "$NC"
+
 if [ -f /etc/os-release ]; then
     source /etc/os-release
 else
@@ -16,88 +33,43 @@ else
     exit 1
 fi
 
-
 OS=($(curl -s "https://api.github.com/repos/flawada/config/contents/blueprints" | grep "name" | cut -d '"' -f 4))
 
 if [[ "${OS[*]}" == *"$ID"* ]]; then
-    printf "%bSupported distro%b\n" "$GREEN" "$NC"
+    printf "%bSystem: %s | supported%b\n" "$GREEN" "$PRETTY_NAME" "$NC"
+else
+    printf "%bSystem: %s | not supported%b\n" "$RED" "$PRETTY_NAME" "$NC"
+    exit 1
 fi
-
 
 blueprints=($(curl -s "https://api.github.com/repos/flawada/config/contents/blueprints/$ID" | grep "name" | cut -d '"' -f 4))
 
+printf "\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n"
 
 printf "%bSelect a config:%b\n" "$BLUE" "$NC"
 select blueprint in "${blueprints[@]}"; do
-    # Check if the user's choice matches an item in our list
     if [ -n "$blueprint" ]; then
         break
     else
-        echo "Invalid choice"
+        printf "%bInvalid choice%b\n" "$RED" "$NC"
     fi
 done
 
+printf "%bConfig selected%b\n" "$GREEN" "$NC"
 
+printf "\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n"
+
+printf "%bDownloading..%b\n" "$BLUE" "$NC"
+
+### install: gum, tar (gunzip)
 curl -sL https://raw.githubusercontent.com/flawada/blueprint/main/blueprints/$ID/$blueprint/files.tar | tar -xf - -C /tmp
 
 cd /tmp/files
 
-### install: gum, tar (gunzip)
-case "$ID" in
-  fedora)
-    printf "%bFedora%b\n" "$GREEN" "$NC"
-    ;;
-  *)
-    printf "%bError: non supported linux distribution. %b\n" "$RED" "$NC"
-    exit 1
-    ;;
-esac
+printf "%bDone%b\n" "$GREEN" "$NC"
+
+printf "\n━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n\n"
+
+printf "%Installing..%b\n" "$BLUE" "$NC"
 
 bash setup.sh
-
-#### deprecated ####
-
-#printf "%s\n" "${blueprints[@]}"
-#blueprint=$(printf "%s\n" "${blueprints[@]}" | gum choose --header "Choose a config:")
-
-#cd /tmp
-#sudo dnf in -y git
-#
-#if ! [ -d "/tmp/$repo" ]; then
-#  git clone https://github.com/flawada/$repo
-#fi
-#
-#if ! [ -d "/tmp/$repo/blueprints/$OS" ]; then
-#  echo "$OS is not supported yet"
-#  echo "suported distros: "
-#  ls /tmp/$repo/blueprints
-#  exit 1
-#fi
-#
-#####
-#
-#clear
-#
-#cd /tmp/$repo/blueprints/$OS
-#
-#blueprints=()
-#i=0
-#
-#for blueprint in */; do
-#  blueprints+=("$blueprint")
-#
-#  echo "$i) ${blueprint%/}"
-#
-#  i=$((i+1))
-#done
-#
-#read -p "select blueprint: " item
-#
-#clear
-#
-#cd ${blueprints[$item]}
-#
-#sudo dnf update -y
-#
-#chmod +x setup.sh
-#./setup.sh
